2d214be0efab81e383be9b5f02cf172958c26263
Aashish Chilwal <aashish001chilwal@gmail.com>
2021-02-06 17:44:02 -0700

    task 3

---
+++ task3.java
@@ -0,0 +1,12 @@
+public class task3 {
+    public static void main(String[] args) {
+        double area, perimeter;
+        double radius = 5.50; 
+        double pi = 3.14;
+         area = pi*radius*radius;
+         perimeter = 2.0*pi*radius;
+        System.out.println("Area: "+area);
+         System.out.println("Perimeter: "+perimeter);
+
+      }
+}

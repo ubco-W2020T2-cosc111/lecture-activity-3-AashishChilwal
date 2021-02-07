6985134caee6a9b7638a54ce24c5e47720b59fcc
Aashish Chilwal <aashish001chilwal@gmail.com>
2021-02-06 18:22:49 -0700

    task 4

---
+++ task4.java
@@ -0,0 +1,7 @@
+public class task4 {
+    public static void main(String []args){
+        double e = 3.0 * 4.0 + 5.0 * 7.1 * 7.2 * 5.5; 
+        double fraction = e % 1;
+           System.out.println("The fractional part of the solution is: "+fraction);
+            }
+}
